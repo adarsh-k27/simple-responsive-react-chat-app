6dc6c94864ec40663a5b8e1e58f645aad61c44f5
adarsh-k27 <adarshkdev27@gmail.com>
2022-12-10 16:04:53 +0530

    fix heroku issue

--- src/axios.js
+++ src/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
-const AxiosInstance=axios.create({baseURL:"https://react-talk-time-chat.herokuapp.com/"})
+const AxiosInstance = axios.create({
+    baseURL: "https://react-chat-app-fgz0.onrender.com/"
+})
 
 export default AxiosInstance;
